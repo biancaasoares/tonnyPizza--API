22835d4f1f70ab8f0d3b01dee1661dfafe56d207
Bianca Soares <biannca01soares@gmail.com>
2023-11-24 12:12:45 -0300

    Em processo

---
+++ modulo/tonyPizzaria.js
@@ -0,0 +1,105 @@
+ var Usuarios = {
+    
+    cadastroUsuario : 'Usuario'
+     [
+        {
+              nome: "Rebeca Tomaz",
+              email: "rebeca@gmail.com",
+              telefone:"11 97696-3167",
+              senha:"123",
+              endereco:""
+
+        },
+        {
+              nome: "Mariana Alves",
+              email: "mariana@gmail.com",
+              telefone:"11 94893-2154",
+              senha:"432",
+              endereco:""
+
+        },
+        {
+              nome: "Vitória Cardoso",
+              email: "vitoria@gmail.com",
+              telefone:"11 98452-4178",
+              senha:"391",
+              endereco:""
+        },
+        {
+              nome: "Luan DSO",
+              email: "luandsoliveira01@gmail.com",
+              telefone:"11 96622-6090",
+              senha:"250",
+              endereco:""
+        }
+     ]
+ };
+
+ var Categorias = {
+    
+    verCategorias: 'Categorias'
+     [
+        {
+            nome: "pizzas",
+            imagem:"",
+            descricao: 
+
+        },
+        {
+            nome: "bebidas",
+            imagem:"",
+            descricao: 
+        },
+        {
+            nome: "frutas",
+            imagem:"",
+            descricao: 
+        },
+        {
+            nome: "sobremesas",
+            imagem:"",
+            descricao: 
+        }
+     ]
+ };
+
+ var Produtos = {
+      verProdutos : 'Produtos'
+      [
+        {
+              nome: "Pizza de calabreza com queijo",
+              descricao:
+              imagem:
+              preco: "16,00"
+              avaliacoes:
+        },
+        {
+            nome: "Pizza de peperoni com queijo",
+            descricao:
+            imagem:
+            preco: "19,00"
+            avaliacoes:
+        },
+        {
+            nome: "Pizza de peperoni com queijo e tomate",
+            descricao:
+            imagem:
+            preco: "16,00"
+            avaliacoes:
+        },
+        {
+            nome: "Pizza brotinho com queijo e tomate",
+            descricao:
+            imagem:
+            preco: "12,00"
+            avaliacoes:
+        },
+        {
+            nome: Pizza de peperoni com queijo,
+            descricao:
+            imagem:
+            preco: "19,00"
+            avaliacoes:
+        },
+      ]
+ }
